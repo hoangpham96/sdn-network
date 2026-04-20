9ac974f4c8a7ec9a3d94512f2f52d49ac3b98f36
Hoang Huy Pham <hoangpham96@gmail.com>
2026-04-20 14:12:31 +1000

    Some changes to the topology

--- topology.py
+++ topology.py
@@ -36,36 +36,22 @@ class SDNTopo(Topo):
         self.addLink(sw4, sw1, bw=100, delay='2ms')   # 100 Mbps
         self.addLink(sw4, sw2, bw=100, delay='2ms')   # 100 Mbps
 
-def run():
+if __name__ == '__main__':
+    setLogLevel('info')
+
     topo = SDNTopo()
+
+    # Initilize Mininet with the topology and controller
     # Use TCLink to support bandwidth and delay parameters
     net = Mininet(topo=topo, controller=RemoteController, switch=OVSSwitch, link=TCLink)
     
     # Start network
     net.start()
 
-    # The assignment mentions "Spanning Tree Protocol (STP) on OVS bridges" as a pointer
-    # So we should run it via mininet to avoid loops, or Ryu can handle STP.
-    # The pointer: "Enable Spanning Tree Protocol (STP) on the OVS bridges (ovs-vsctl set bridge s1 stp_enable=true)"
+    # Enable Spanning Tree Protocol (STP) on the OVS bridges (ovs-vsctl set bridge s1 stp_enable=true)"
     for sw in net.switches:
         sw.cmd('ovs-vsctl set bridge {} stp_enable=true'.format(sw.name))
-
-    # A2. Host and Server Setup
-    info('*** Starting simple HTTP server on Server\n')
-    server = net.get('server')
-    # Run the HTTP server in the background
-    server.cmd('python3 -m http.server 80 &')
-
-    # A3. Topology Correctness
-    info('*** Topology is running. Use net or dump to verify.\n')
     
     CLI(net)
 
-    # Teardown
-    info('*** Stopping HTTP server\n')
-    server.cmd('kill %python3')
     net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info')
-    run()
